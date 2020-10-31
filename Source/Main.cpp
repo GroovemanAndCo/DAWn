005e9aac9dda096487b4b44413459ab6b5119898
fabien <fabien@onepost.net>
2020-10-30 20:32:56 -0500

    More GPL headers updates.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,4 +1,21 @@
 /*
+ * This file is part of the DAWn distribution (https://github.com/GroovemanAndCo/DAWn).
+ * Copyright (c) 2020 Fabien (https://github.com/fab672000)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /*
   ==============================================================================
 
     This file contains the basic startup code for a JUCE application.
