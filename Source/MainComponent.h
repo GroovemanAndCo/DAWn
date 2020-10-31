c9680d8de2b3d2e61f179b40537b84b3d5bb6002
fabien <fabien@onepost.net>
2020-10-30 20:30:26 -0500

    Update GPL headers

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,4 +1,21 @@
-/*******************************************************************************
+/*
+ * This file is part of the DAWn distribution (https://github.com/GroovemanAndCo/DAWn).
+ * Copyright (c) 2020 Fabien (https://github.com/fab672000)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /*******************************************************************************
  The block below describes the properties of this PIP. A PIP is a short snippet
  of code that can be read by the Projucer and used to generate a JUCE project.
 
